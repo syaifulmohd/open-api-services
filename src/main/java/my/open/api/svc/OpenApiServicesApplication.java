4c7ed502e732c19b74c00f344371b2aa1b7b0544
syaifulmohd <syaiful.arqam@gmail.com>
2020-09-21 02:52:14 +0800

    new commit

---
+++ src/main/java/my/open/api/svc/OpenApiServicesApplication.java
@@ -0,0 +1,13 @@
+package my.open.api.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/open/api/svc/OpenApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package my.open.api.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
